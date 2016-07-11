2eebdea79bc5ca82ff68079ede9813c74a7204f0
MexiCANIENSE <gothardpr@gmail.com>
2016-07-10 22:25:18 -0500

    version nueva

--- app/src/main/java/com/edu/paytonramirezg/clinchacha/Screen1.java
+++ app/src/main/java/com/edu/paytonramirezg/clinchacha/Screen1.java
@@ -43,7 +43,33 @@ public class Screen1 extends AppCompatActivity {
 
 
 
+//request Location Permissuin
+        if (ContextCompat.checkSelfPermission(Screen1.this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(Screen1.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(Screen1.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
 
 
         Thread timerThread = new Thread(){
@@ -58,33 +84,7 @@ public class Screen1 extends AppCompatActivity {
 
                     if(userProfile.contains("Token")){
 
-                     //request Location Permissuin
-                        if (ContextCompat.checkSelfPermission(Screen1.this,
-                                Manifest.permission.ACCESS_FINE_LOCATION)
-                                != PackageManager.PERMISSION_GRANTED) {
 
-                            // Should we show an explanation?
-                            if (ActivityCompat.shouldShowRequestPermissionRationale(Screen1.this,
-                                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-                                // Show an expanation to the user *asynchronously* -- don't block
-                                // this thread waiting for the user's response! After the user
-                                // sees the explanation, try again to request the permission.
-
-                            } else {
-
-                                // No explanation needed, we can request the permission.
-
-                                ActivityCompat.requestPermissions(Screen1.this,
-                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-
-
-                                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                                // app-defined int constant. The callback method gets the
-                                // result of the request.
-                            }
-                        }
 
 
 
